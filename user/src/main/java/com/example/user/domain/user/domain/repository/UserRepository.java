9a2788ed80d316561109be43182c65f53beb7497
ljhy07 <ij184399@gmail.com>
2025-02-19 19:55:20 +0900

    add(user): UserRepository JPA 방식으로 추가

---
+++ user/src/main/java/com/example/user/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.user.domain.user.domain.repository;
+
+import com.example.user.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
