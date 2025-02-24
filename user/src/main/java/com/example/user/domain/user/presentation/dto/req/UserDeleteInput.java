113eda14f689e3559dd7f0fa19080a1034af60e0
ljhy07 <ij184399@gmail.com>
2025-02-24 20:52:35 +0900

    add(user): 필요없는 컬럼 삭제

--- user/src/main/java/com/example/user/domain/user/presentation/dto/req/UserDeleteInput.java
+++ user/src/main/java/com/example/user/domain/user/presentation/dto/req/UserDeleteInput.java
@@ -1,7 +1,6 @@
 package com.example.user.domain.user.presentation.dto.req;
 
 public record UserDeleteInput(
-        Long id,
         String accessToken
 ) {
 }
--- user/src/main/java/com/example/user/domain/user/presentation/dto/req/UserPasswordUpdateInput.java
+++ user/src/main/java/com/example/user/domain/user/presentation/dto/req/UserPasswordUpdateInput.java
@@ -1,7 +1,7 @@
 package com.example.user.domain.user.presentation.dto.req;
 
 public record UserPasswordUpdateInput(
-        Long userId,
+        String accessToken,
         String password
 ) {
 }
