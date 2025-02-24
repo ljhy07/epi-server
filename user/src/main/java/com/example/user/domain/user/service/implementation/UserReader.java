5ec32fd1dac180d1fc314eaa21822fe72e07cbac
ljhy07 <ij184399@gmail.com>
2025-02-24 20:30:45 +0900

    add(user): reader 추가

---
+++ user/src/main/java/com/example/user/domain/user/service/implementation/UserReader.java
@@ -0,0 +1,24 @@
+package com.example.user.domain.user.service.implementation;
+
+import com.example.user.domain.user.domain.User;
+import com.example.user.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserReader {
+
+    private final UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElse(null);
+    }
+}
