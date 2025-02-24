c6bfe3e21f62e42ed51bc7587504bfae0f7493ff
ljhy07 <ij184399@gmail.com>
2025-02-24 21:00:58 +0900

    add(user): 컨트롤러와 서비스 연결 코드 추가

--- user/src/main/java/com/example/user/domain/user/presentation/UserController.java
+++ user/src/main/java/com/example/user/domain/user/presentation/UserController.java
@@ -6,6 +6,8 @@ import com.example.user.domain.user.presentation.dto.req.UserPasswordUpdateInput
 import com.example.user.domain.user.presentation.dto.req.UserUpdateInput;
 import com.example.user.domain.user.presentation.dto.res.UserCommandResponse;
 import com.example.user.domain.user.presentation.dto.res.UserQueryResponse;
+import com.example.user.domain.user.service.CommandUserService;
+import com.example.user.domain.user.service.QueryUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -18,51 +20,54 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final CommandUserService commandUserService;
+    private final QueryUserService queryUserService;
+
     @QueryMapping
     public List<UserQueryResponse> getAllUser(){
-
+        return queryUserService.getUsers();
     }
 
     @QueryMapping
     public UserQueryResponse getUserById(
             @Argument Long userId
     ){
-
+        return queryUserService.getUser(userId);
     }
 
     @QueryMapping
     public UserQueryResponse getUserByAccessToken(
             @Argument String accessToken
     ){
-
+        return queryUserService.getUserByAccessToken(accessToken);
     }
 
     @MutationMapping
     public UserCommandResponse createUser(
             @Argument UserCreateInput userCreateInput
     ){
-
+        return commandUserService.save(userCreateInput);
     }
 
     @MutationMapping
     public UserCommandResponse updateUser(
             @Argument UserUpdateInput userUpdateInput
     ){
-
+        return commandUserService.update(userUpdateInput);
     }
 
     @MutationMapping
     public UserCommandResponse updateUserPassword(
             @Argument UserPasswordUpdateInput userPasswordUpdateInput
     ){
-
+        return commandUserService.updatePassword(userPasswordUpdateInput);
     }
 
     @MutationMapping
     public UserCommandResponse deleteUser(
             @Argument UserDeleteInput userDeleteInput
     ){
-
+        return commandUserService.delete(userDeleteInput);
     }
 
 
