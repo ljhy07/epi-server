90dc6177984c20fac4332858dd51db87bc5570da
ljhy07 <ij184399@gmail.com>
2025-02-26 21:17:13 +0900

    add(auth): refresh 토큰용 엔티티 추가

---
+++ user/src/main/java/com/example/user/domain/auth/domain/Refresh.java
@@ -0,0 +1,24 @@
+package com.example.user.domain.auth.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "Refresh")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Refresh {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String refreshToken;
+
+    private String expiration;
+}
