5f34f190b3b85d6463dd909087674c9ab84e0525
ljhy07 <ij184399@gmail.com>
2025-02-27 22:18:31 +0900

    fix(user): RestController로 변경

--- user/src/main/java/com/example/user/domain/user/presentation/UserController.java
+++ user/src/main/java/com/example/user/domain/user/presentation/UserController.java
@@ -12,11 +12,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
 
