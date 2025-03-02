a070570f13c1d88faaf1e70f51edd5be091621e5
ljhy07 <ij184399@gmail.com>
2025-03-02 22:35:16 +0900

    add(user): email로 찾는 메소드 추가

--- user/src/main/java/com/example/user/domain/user/domain/repository/UserRepository.java
+++ user/src/main/java/com/example/user/domain/user/domain/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.user.domain.user.domain.repository;
 
 import com.example.user.domain.user.domain.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(@Email String email);
 }
