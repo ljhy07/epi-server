2066acc888c21a921351e3d610f1961619ace5f8
이재현 <2023050@bssm.hs.kr>
2025-03-07 19:46:18 +0900

    add(global): 에러 반환 dto

---
+++ user/src/main/java/com/example/user/global/exception/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.example.user.global.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        int status,
+        String errorCode,
+        String message,
+        LocalDateTime timestamp
+) {
+    public static ErrorResponse from(UserException errorCode) {
+        return new ErrorResponse(
+                errorCode.getStatus().value(),
+                errorCode.getErrorCode(),
+                errorCode.getMessage(),
+                LocalDateTime.now()
+        );
+    }
+
+    public static ErrorResponse from(int status, String errorCode, String message) {
+        return new ErrorResponse(
+                status,
+                errorCode,
+                message,
+                LocalDateTime.now()
+        );
+    }
+}
