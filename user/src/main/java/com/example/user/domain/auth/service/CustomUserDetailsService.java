82dd6d0a0e651a1fa48fdc568e03ae9d5fae98f8
이재현 <2023050@bssm.hs.kr>
2025-03-11 19:58:13 +0900

    fix(user): 반환 타입 변경으로 user 검사 코드 변경 및 Exception 추가

--- user/src/main/java/com/example/user/domain/auth/service/CustomUserDetailsService.java
+++ user/src/main/java/com/example/user/domain/auth/service/CustomUserDetailsService.java
@@ -3,11 +3,14 @@ package com.example.user.domain.auth.service;
 import com.example.user.domain.auth.service.dto.CustomUserDetails;
 import com.example.user.domain.user.domain.User;
 import com.example.user.domain.user.domain.repository.UserRepository;
+import com.example.user.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +19,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        User user = userRepository.findByEmail(email);
+        Optional<User> user = userRepository.findByEmail(email);
 
-        if (user != null) {
-            return new CustomUserDetails(user);
-        }
-
-        return null;
+        return user.map(CustomUserDetails::new)
+                .orElseThrow(UserNotFoundException::new);
     }
 }
