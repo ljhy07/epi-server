5a33eae8e7f95f9840c66a3aafad5d9747e1a48a
이재현 <2023050@bssm.hs.kr>
2025-03-11 19:53:00 +0900

    fix(user): findByEmail Optional<User>로 반환 타입 수정

--- user/src/main/java/com/example/user/domain/user/domain/repository/UserRepository.java
+++ user/src/main/java/com/example/user/domain/user/domain/repository/UserRepository.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(@Email String email);
+    Optional<User> findByEmail(@Email String email);
 }
