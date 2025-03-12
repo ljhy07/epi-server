298dfc4c1b18a1b8fed4c3a913a4fc0e953dceae
이재현 <2023050@bssm.hs.kr>
2025-03-12 09:28:29 +0900

    feat(oauth): 구글 OAuth 유저 등록

---
+++ user/src/main/java/com/example/user/domain/auth/service/implementation/google/GoogleAuthService.java
@@ -0,0 +1,51 @@
+package com.example.user.domain.auth.service.implementation.google;
+
+import com.example.user.domain.auth.presentation.dto.req.OAuthTokenLoginInput;
+import com.example.user.domain.auth.presentation.dto.res.Token;
+import com.example.user.domain.user.domain.User;
+import com.example.user.domain.user.domain.value.LoginType;
+import com.example.user.domain.user.domain.value.Role;
+import com.example.user.domain.user.presentation.dto.res.UserQueryResponse;
+import com.example.user.domain.user.service.CommandUserService;
+import com.example.user.domain.user.service.QueryUserService;
+import com.example.user.global.feign.auth.google.GoogleInformationClient;
+import com.example.user.global.feign.auth.google.dto.res.GoogleInformationResponse;
+import com.example.user.global.jwt.util.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleAuthService {
+
+    private final GoogleInformationClient googleInformationClient;
+    private final CommandUserService commandUserService;
+    private final QueryUserService queryUserService;
+    private final JwtUtils jwtUtils;
+
+    @Transactional
+    public Token execute(OAuthTokenLoginInput oauthTokenLoginInput) {
+        GoogleInformationResponse response = googleInformationClient
+                .getUserInformation(oauthTokenLoginInput.accessToken());
+        String email = response.getEmail();
+
+        UserQueryResponse user = queryUserService.getUserByEmail(email);
+
+        if (user == null) {
+            commandUserService.saveOAuth(
+                    User.oauthUserCreateBuilder()
+                        .name(response.getName())
+                        .email(email)
+                        .loginType(LoginType.GOOGLE)
+                        .role(Role.User)
+                        .build()
+            );
+        }
+
+        return new Token(
+                jwtUtils.getRefreshToken(email),
+                jwtUtils.getAccessToken(email)
+        );
+    }
+}
