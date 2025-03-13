6401ff096ab492df504ba5accb0c0e37e200481c
이재현 <2023050@bssm.hs.kr>
2025-03-13 19:57:52 +0900

    refactor(user): 유저 생성 AuthController로 이동

--- user/src/main/java/com/example/user/domain/user/presentation/UserController.java
+++ user/src/main/java/com/example/user/domain/user/presentation/UserController.java
@@ -1,18 +1,15 @@
 package com.example.user.domain.user.presentation;
 
 import com.example.user.domain.user.presentation.dto.req.UserCreateInput;
-import com.example.user.domain.user.presentation.dto.req.UserDeleteInput;
 import com.example.user.domain.user.presentation.dto.req.UserPasswordUpdateInput;
 import com.example.user.domain.user.presentation.dto.req.UserUpdateInput;
-import com.example.user.domain.user.presentation.dto.res.UserCommandResponse;
-import com.example.user.domain.user.presentation.dto.res.UserQueryResponse;
+import com.example.user.domain.user.presentation.dto.res.UserResponse;
 import com.example.user.domain.user.service.CommandUserService;
 import com.example.user.domain.user.service.QueryUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,45 +22,45 @@ public class UserController {
     private final QueryUserService queryUserService;
 
     @QueryMapping
-    public List<UserQueryResponse> getAllUser(){
+    public List<UserResponse> getAllUser(){
         return queryUserService.getUsers();
     }
 
     @QueryMapping
-    public UserQueryResponse getUserById(
+    public UserResponse getUserById(
             @Argument Long userId
     ){
         return queryUserService.getUser(userId);
     }
 
     @QueryMapping
-    public UserQueryResponse getUserByAccessToken(){
+    public UserResponse getUserByAccessToken(){
         return queryUserService.getUserByAccessToken();
     }
 
-    @MutationMapping
-    public UserCommandResponse createUser(
-            @Argument UserCreateInput userCreateInput
-    ){
-        return commandUserService.save(userCreateInput);
-    }
+//    @MutationMapping
+//    public UserResponse createUser(
+//            @Argument UserCreateInput userCreateInput
+//    ){
+//        return commandUserService.save(userCreateInput);
+//    }
 
     @MutationMapping
-    public UserCommandResponse updateUser(
+    public UserResponse updateUser(
             @Argument UserUpdateInput userUpdateInput
     ){
         return commandUserService.update(userUpdateInput);
     }
 
     @MutationMapping
-    public UserCommandResponse updateUserPassword(
+    public UserResponse updateUserPassword(
             @Argument UserPasswordUpdateInput userPasswordUpdateInput
     ){
         return commandUserService.updatePassword(userPasswordUpdateInput);
     }
 
     @MutationMapping
-    public UserCommandResponse deleteUser(){
+    public UserResponse deleteUser(){
         return commandUserService.delete();
     }
 
