6eaba6c75645e00fd2a4c599ea9b1e3633536d70
이재현 <2023050@bssm.hs.kr>
2025-03-13 20:02:37 +0900

    chore(user): 반환 타입 수정

--- user/src/main/java/com/example/user/domain/user/service/QueryUserService.java
+++ user/src/main/java/com/example/user/domain/user/service/QueryUserService.java
@@ -1,7 +1,7 @@
 package com.example.user.domain.user.service;
 
 import com.example.user.domain.user.domain.User;
-import com.example.user.domain.user.presentation.dto.res.UserQueryResponse;
+import com.example.user.domain.user.presentation.dto.res.UserResponse;
 import com.example.user.domain.user.service.implementation.UserReader;
 import com.example.user.global.jwt.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
@@ -17,16 +17,19 @@ public class QueryUserService {
 
     private final UserReader userReader;
 
-    public List<UserQueryResponse> getUsers(){
+    public List<UserResponse> getUsers(){
         List<User> users = userReader.findAll();
-        List<UserQueryResponse> userQueryResponses = new ArrayList<>();
+        List<UserResponse> userQueryResponses = new ArrayList<>();
 
         for (User user : users) {
             userQueryResponses.add(
-                    new UserQueryResponse(
+                    new UserResponse(
                             user.getId(),
                             user.getName(),
-                            user.getEmail()
+                            user.getEmail(),
+                            user.getPhone(),
+                            user.getLoginType(),
+                            user.getRole()
                     )
             );
         }
@@ -34,37 +37,46 @@ public class QueryUserService {
         return  userQueryResponses;
     }
 
-    public UserQueryResponse getUser(
+    public UserResponse getUser(
             Long userId
     ){
         User user = userReader.findById(userId);
 
-        return new UserQueryResponse(
+        return new UserResponse(
                 user.getId(),
                 user.getName(),
-                user.getEmail()
+                user.getEmail(),
+                user.getPhone(),
+                user.getLoginType(),
+                user.getRole()
         );
     }
 
-    public UserQueryResponse getUserByAccessToken(){
+    public UserResponse getUserByAccessToken(){
         User user = userReader.findByEmailFromSecurity();
 
-        return new UserQueryResponse(
+        return new UserResponse(
                 user.getId(),
                 user.getName(),
-                user.getEmail()
+                user.getEmail(),
+                user.getPhone(),
+                user.getLoginType(),
+                user.getRole()
         );
     }
 
-    public UserQueryResponse getUserByEmail(
+    public UserResponse getUserByEmail(
             String email
     ){
         User user = userReader.findByEmail(email);
 
-        return new UserQueryResponse(
+        return new UserResponse(
                 user.getId(),
                 user.getName(),
-                user.getEmail()
+                user.getEmail(),
+                user.getPhone(),
+                user.getLoginType(),
+                user.getRole()
         );
     }
 }
