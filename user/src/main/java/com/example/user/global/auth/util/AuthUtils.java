b5d22bb2c4b8e7febb089437ab616a6e2117e0a8
이재현 <2023050@bssm.hs.kr>
2025-03-20 08:47:21 +0900

    feat(auth): Spring Security에서 유저 정보 꺼내는 메소드 추가

---
+++ user/src/main/java/com/example/user/global/auth/util/AuthUtils.java
@@ -0,0 +1,27 @@
+package com.example.user.global.auth.util;
+
+import com.example.user.domain.user.domain.User;
+import com.example.user.domain.user.service.implementation.UserReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthUtils {
+
+    private final UserReader userReader;
+
+    public User getCurrentUser() {
+        return userReader.findByEmail(getCurrentUserEmail());
+    }
+
+    private String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof String) {
+            return (String) authentication.getPrincipal();
+        }
+        throw new IllegalStateException("User ID not found in Security Context");
+    }
+}
