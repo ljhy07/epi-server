4aa16dcfbaa95e38dab35ed0932e7990451dc8e9
이재현 <2023050@bssm.hs.kr>
2025-03-21 14:04:48 +0900

    feat(email): 이메일 scheduler 추가

---
+++ user/src/main/java/com/example/user/domain/email/service/scheduler/EmailScheduler.java
@@ -0,0 +1,22 @@
+package com.example.user.domain.email.service.scheduler;
+
+import com.example.user.domain.email.domain.repository.EmailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private final EmailRepository emailRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void cleanupExpiredTokens() {
+        emailRepository.deleteByExpiryDateBefore(LocalDateTime.now());
+    }
+}
