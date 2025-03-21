29559cbba3908b0a05b10ee4d0bb2b8cf3fa24da
이재현 <2023050@bssm.hs.kr>
2025-03-21 14:09:17 +0900

    feat(auth): google api v1 -> v2로 변경

--- user/src/main/java/com/example/user/global/feign/auth/google/GoogleInformationClient.java
+++ user/src/main/java/com/example/user/global/feign/auth/google/GoogleInformationClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "GoogleInformationClient", url = "https://www.googleapis.com/oauth2/v1/userinfo")
+@FeignClient(name = "GoogleInformationClient", url = "https://www.googleapis.com/oauth2/v2/userinfo")
 public interface GoogleInformationClient {
     @GetMapping("?alt=json&access_token={TOKEN}")
     GoogleInformationResponse getUserInformation(@PathVariable("TOKEN") String accessToken);
