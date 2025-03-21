7be5444bf32aeb85178384f89a1f12141221105a
이재현 <2023050@bssm.hs.kr>
2025-03-21 14:01:27 +0900

    feat(email): JPA 쿼리 추가

--- user/src/main/java/com/example/user/domain/email/domain/repository/EmailRepository.java
+++ user/src/main/java/com/example/user/domain/email/domain/repository/EmailRepository.java
@@ -4,6 +4,15 @@ import com.example.user.domain.email.domain.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface EmailRepository extends JpaRepository<Email, Long> {
+    void deleteByExpiryDateBefore(LocalDateTime expiryDateBefore);
+
+    void deleteByEmail(String email);
+
+    Email findByEmailAndIsVerified(String email, Boolean b);
+
+    Email findByToken(String token);
 }
