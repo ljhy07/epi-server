d9e21ff0fbd94a9e633a2d31cd6e2ade4403bb86
이재현 <2023050@bssm.hs.kr>
2025-03-21 17:25:45 +0900

    feat(address): 주소 컨트롤러

---
+++ user/src/main/java/com/example/user/domain/address/presentation/AddressController.java
@@ -0,0 +1,58 @@
+package com.example.user.domain.address.presentation;
+
+import com.example.user.domain.address.presentation.dto.req.AddressInput;
+import com.example.user.domain.address.presentation.dto.req.CreateAddressInput;
+import com.example.user.domain.address.presentation.dto.req.DeleteAddressInput;
+import com.example.user.domain.address.presentation.dto.req.UpdateAddressInput;
+import com.example.user.domain.address.presentation.dto.res.AddressResponse;
+import com.example.user.domain.address.service.MutationAddressService;
+import com.example.user.domain.address.service.QueryAddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final MutationAddressService mutationAddressService;
+    private final QueryAddressService queryAddressService;
+
+    @QueryMapping
+    public AddressResponse getAddressByAddressId(
+            @Argument(name = "addressInput") AddressInput addressInput
+    ){
+        return queryAddressService.getAddressByAddressId(addressInput);
+    }
+
+    @QueryMapping
+    public List<AddressResponse> getAddressesByUserId(){
+        return queryAddressService.getAddressesByUserId();
+    }
+
+    @MutationMapping
+    public AddressResponse createAddress(
+            @Argument(name = "createAddressInput") CreateAddressInput createAddressInput
+    ){
+        return mutationAddressService.createAddress(createAddressInput);
+    }
+
+    @MutationMapping
+    public AddressResponse updateAddress(
+            @Argument(name = "updateAddressInput") UpdateAddressInput updateAddressInput
+    ){
+        return mutationAddressService.updateAddress(updateAddressInput);
+    }
+
+    @MutationMapping
+    public AddressResponse deleteAddress(
+            @Argument(name = "deleteAddressInput") DeleteAddressInput deleteAddressInput
+    ){
+        return mutationAddressService.deleteAddress(deleteAddressInput);
+    }
+
+}
