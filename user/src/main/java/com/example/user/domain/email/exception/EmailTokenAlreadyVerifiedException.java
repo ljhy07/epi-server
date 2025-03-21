d3802cef5ca5163d848ffed95d807831a615d848
이재현 <2023050@bssm.hs.kr>
2025-03-21 14:05:20 +0900

    feat(email): 이메일 인증된 토큰 예외

---
+++ user/src/main/java/com/example/user/domain/email/exception/EmailTokenAlreadyVerifiedException.java
@@ -0,0 +1,10 @@
+package com.example.user.domain.email.exception;
+
+import com.example.user.global.exception.mail.UserMailException;
+import org.springframework.http.HttpStatus;
+
+public class EmailTokenAlreadyVerifiedException extends UserMailException {
+    public EmailTokenAlreadyVerifiedException() {
+        super(HttpStatus.BAD_REQUEST, "EMAIL_TOKEN_ALREADY_VERIFIED", "이미 인증된 토큰입니다.");
+    }
+}
