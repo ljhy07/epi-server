93140cc73d46088d1a31cf8b4e2e3074174f5a40
이재현 <2023050@bssm.hs.kr>
2025-03-21 14:05:30 +0900

    feat(email): 이메일 전송 실패 예외

---
+++ user/src/main/java/com/example/user/domain/email/exception/EmailSendFailedException.java
@@ -0,0 +1,10 @@
+package com.example.user.domain.email.exception;
+
+import com.example.user.global.exception.mail.UserMailException;
+import org.springframework.http.HttpStatus;
+
+public class EmailSendFailedException extends UserMailException {
+    public EmailSendFailedException() {
+        super(HttpStatus.INTERNAL_SERVER_ERROR, "EMAIL_SEND_FAILED", "이메일 전송에 실패했습니다.");
+    }
+}
