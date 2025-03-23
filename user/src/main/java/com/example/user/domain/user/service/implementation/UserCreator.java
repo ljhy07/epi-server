800bcbf476139deb68d076e7c53a6624b6961863
이재현 <2023050@bssm.hs.kr>
2025-03-23 22:42:15 +0900

    feat(user): 추가 정보 저장하는 메소드 추가

--- user/src/main/java/com/example/user/domain/user/service/implementation/UserCreator.java
+++ user/src/main/java/com/example/user/domain/user/service/implementation/UserCreator.java
@@ -1,5 +1,6 @@
 package com.example.user.domain.user.service.implementation;
 
+import com.example.user.domain.auth.presentation.dto.req.AdditionalInfoInput;
 import com.example.user.domain.user.domain.User;
 import com.example.user.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,15 @@ public class UserCreator {
     private final UserRepository userRepository;
 
     public User create(User user){
+        return userRepository.save(user);
+    }
+
+    public User additionalInfo(User user, AdditionalInfoInput additionalInfoInput){
+        user.additionalInfoBuilder()
+                .name(additionalInfoInput.name())
+                .phone(additionalInfoInput.phone())
+                .build();
+
         return userRepository.save(user);
     }
 }
