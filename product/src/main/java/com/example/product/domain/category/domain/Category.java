bc966a2237b59dadb1cde357416afe1d9eb81354
이재현 <2023050@bssm.hs.kr>
2025-03-30 22:14:54 +0900

    feat(category): builder 및 컬럼 추가

--- product/src/main/java/com/example/product/domain/category/domain/Category.java
+++ product/src/main/java/com/example/product/domain/category/domain/Category.java
@@ -1,12 +1,15 @@
 package com.example.product.domain.category.domain;
 
+import com.example.product.domain.category.domain.value.TopCategory;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.kafka.streams.processor.To;
 
 @Getter
 @Entity(name = "category")
@@ -18,6 +21,22 @@ public class Category {
 
     private Long productId;
 
-    private String categoryName;
+    private TopCategory topCategory;
+
+    private String normalCategory;
+
+
+    @Builder(builderMethodName = "createBuilder")
+    public Category(Long productId, TopCategory topCategory, String normalCategory) {
+        this.productId = productId;
+        this.topCategory = topCategory;
+        this.normalCategory = normalCategory;
+    }
+
+    @Builder(builderMethodName = "updateBuilder")
+    public Category(TopCategory topCategory, String normalCategory) {
+        this.topCategory = topCategory;
+        this.normalCategory = normalCategory;
+    }
 
 }
