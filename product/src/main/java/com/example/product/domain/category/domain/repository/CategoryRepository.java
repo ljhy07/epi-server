6a8ca06ec0cc266854aaae9e7770fcc2b7b1da92
이재현 <2023050@bssm.hs.kr>
2025-04-01 09:24:14 +0900

    feat(product&category): @Repository 추가

--- product/src/main/java/com/example/product/domain/category/domain/repository/CategoryRepository.java
+++ product/src/main/java/com/example/product/domain/category/domain/repository/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.example.product.domain.category.domain.repository;
 import com.example.product.domain.category.domain.Category;
 import com.example.product.domain.category.domain.value.TopCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByCategoryId(Long categoryId);
 
--- product/src/main/java/com/example/product/domain/product/domain/repository/ProductRepository.java
+++ product/src/main/java/com/example/product/domain/product/domain/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package com.example.product.domain.product.domain.repository;
 
 import com.example.product.domain.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByProductId(Long productId);
 
