fe90cceed6cd5cdb03406f06bb9784a83eae34c6
이재현 <2023050@bssm.hs.kr>
2025-04-05 11:48:59 +0900

    feat(store): repository 등록

---
+++ store/src/main/java/com/example/store/domain/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.example.store.domain.repository;
+
+import com.example.store.domain.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
