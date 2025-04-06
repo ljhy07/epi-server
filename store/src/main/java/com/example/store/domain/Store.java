a2dd289cbffc422bc7918dc7f1dff2912a6353d5
이재현 <2023050@bssm.hs.kr>
2025-04-06 15:09:43 +0900

    feat(store): builder add

--- store/src/main/java/com/example/store/domain/Store.java
+++ store/src/main/java/com/example/store/domain/Store.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,10 +17,27 @@ public class Store {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long storeId;
 
+    private Long userId;
+
     private String storeName;
 
     private String storeAddress;
 
     private String storeDescription;
+
+    @Builder(builderMethodName = "createStoreBuilder")
+    public Store(Long userId, String storeName, String storeAddress, String storeDescription) {
+        this.userId = userId;
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.storeDescription = storeDescription;
+    }
+
+    @Builder(builderMethodName = "updateStoreBuilder")
+    public Store(String storeName, String storeAddress, String storeDescription, Long storeId) {
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.storeDescription = storeDescription;
+    }
 
 }
