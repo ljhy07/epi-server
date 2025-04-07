8b64e8d7148c66377e8bb8bcda8e18898d0044ef
이재현 <2023050@bssm.hs.kr>
2025-04-07 23:45:54 +0900

    feat(store): 레포지토리 쿼리 추가

--- store/src/main/java/com/example/store/domain/repository/StoreRepository.java
+++ store/src/main/java/com/example/store/domain/repository/StoreRepository.java
@@ -4,6 +4,13 @@ import com.example.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Store findByUserId(Long userId);
+
+    Store findByStoreId(Long storeId);
+
+    List<Store> findAllByStoreName(String storeName);
 }
