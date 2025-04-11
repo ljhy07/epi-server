5db2878ecb0b1b67e81cfd3a2da1f63a40fb865e
이재현 <ij184399@gmail.com>
2025-04-11 23:11:13 +0900

    feat(review): read query function add

--- review/src/main/java/com/example/review/domain/repository/ReviewRepository.java
+++ review/src/main/java/com/example/review/domain/repository/ReviewRepository.java
@@ -3,5 +3,12 @@ package com.example.review.domain.repository;
 import com.example.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Review findByReviewId(Long reviewId);
+
+    List<Review> findAllByProductId(Long productId);
+
+    List<Review> findAllByUserId(Long userId);
 }
