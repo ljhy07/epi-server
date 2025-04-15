e85069cc94eaf94ea7a16a98f235c05de89ee4b0
이재현 <ij184399@gmail.com>
2025-04-16 08:19:06 +0900

    feat(review): @Repository add

--- review/src/main/java/com/example/review/domain/repository/ReviewRepository.java
+++ review/src/main/java/com/example/review/domain/repository/ReviewRepository.java
@@ -2,9 +2,11 @@ package com.example.review.domain.repository;
 
 import com.example.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Review findByReviewId(Long reviewId);
 
