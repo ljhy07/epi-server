db528a33654d5150d265fc6d8717dc46de86af26
이재현 <ij184399@gmail.com>
2025-04-19 23:23:11 +0900

    feat(reservation): 예약 시 결제 여부 추가

---
+++ reservation/src/main/java/com/example/reservation/domain/value/PaymentStatus.java
@@ -0,0 +1,28 @@
+package com.example.reservation.domain.value;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus{
+    Online("online"),
+    Offline("offline");
+
+    private final String paymentStatus;
+
+    PaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String PaymentStatus() {
+        return paymentStatus;
+    }
+
+    public static PaymentStatus fromValue(String value) {
+        for (PaymentStatus paymentStatus : PaymentStatus.values()) {
+            if (paymentStatus.name().equalsIgnoreCase(value) || paymentStatus.PaymentStatus().equalsIgnoreCase(value)) {
+                return paymentStatus;
+            }
+        }
+        throw new IllegalArgumentException("Invalid paymentStatus value: " + value);
+    }
+}
