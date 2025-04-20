b9e65737f7711d809932bb5646541035ad38bf9a
이재현 <ij184399@gmail.com>
2025-04-20 23:30:38 +0900

    feat(reservation): payment status logic update in updater

--- reservation/src/main/java/com/example/reservation/service/implementation/ReservationUpdater.java
+++ reservation/src/main/java/com/example/reservation/service/implementation/ReservationUpdater.java
@@ -2,9 +2,11 @@ package com.example.reservation.service.implementation;
 
 import com.example.reservation.domain.Reservation;
 import com.example.reservation.domain.repository.ReservationRepository;
+import com.example.reservation.presentation.dto.req.ReservationInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Service
@@ -13,9 +15,12 @@ public class ReservationUpdater {
 
     private final ReservationRepository reservationRepository;
 
-    public Reservation update(Reservation updatableReservation, Date date) {
+    public Reservation update(Reservation updatableReservation, ReservationInput reservationInput) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
         updatableReservation.updateBuilder()
-                .date(date)
+                .paymentStatus(reservationInput.paymentStatus())
+                .date(dateFormat.format(new Date()))
                 .build();
 
         return reservationRepository.save(updatableReservation);
