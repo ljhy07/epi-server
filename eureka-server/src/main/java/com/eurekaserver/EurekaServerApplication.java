134c4b1c23fa95cef97327fe0f841710f758e713
이재현 <ij184399@gmail.com>
2025-04-25 08:20:10 +0900

    feat(eureka): Eureka 서버 구현 완료

--- eureka-server/src/main/java/com/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eurekaserver/EurekaServerApplication.java
@@ -1,8 +1,10 @@
-package com.example.eurekaserver;
+package com.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
