7efd76d31af4fd547d9f4fd3bf822574ba2a10f0
이재현 <ij184399@gmail.com>
2025-04-28 07:58:56 +0900

    feat(user): user name의 unique 제거

--- user/src/main/java/com/example/user/domain/user/domain/User.java
+++ user/src/main/java/com/example/user/domain/user/domain/User.java
@@ -17,7 +17,7 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 15, nullable = false, unique = true)
+    @Column(length = 15, nullable = false)
     private String name;
 
     private String password;
